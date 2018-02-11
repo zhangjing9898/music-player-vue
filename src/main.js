391b2c6debf8d9ae56924e724adfa2f616da545d
zhangjing9898 <1105249307@qq.com>
2018-02-11 11:18:52 +0800

    music-player
    基于vue做的一个防qq音乐的音乐播放器

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+import './common/stylus/index.styl';
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
