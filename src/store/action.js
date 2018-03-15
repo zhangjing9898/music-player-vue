6f198584bcb305d169b53d382f487bfdf097325c
zhangjing9898 <1105249307@qq.com>
2018-03-15 18:40:55 +0800

    playervuex

--- src/store/action.js
+++ src/store/action.js
@@ -1,1 +1,10 @@
+import * as types from './mutation-types';
+import {playMode} from '../common/js/config';
 
+//commit 用来触发同步操作的方法
+export const selectPlay=function ({commit,state},{list,index}) {
+  commit(types.SET_SEQUENCE_LIST,list);
+  commit(types.SET_CURRENT_INDEX,index);
+  commit(types.SET_FULL_SCREEN,true);
+  commit(types.SET_PLAYING_STATE,true)
+}
